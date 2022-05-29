ae289995d7a822bfbe566c95f3e385e9ae4ac23b
Jitesh Rajamanikyam Udayakumar <jrajaman@uwaterloo.ca>
2022-05-29 01:25:51 -0400

    CURD for Posts Model

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgon = require("morgan");
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
+const postsRoute = require("./routes/posts")
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(morgon("common"));
 
 app.use("/api/users",userRoute)
 app.use("/api/auth",authRoute)
+app.use("/api/posts",postsRoute)
 
 app.listen(8800,()=>{
     console.log("Backend server is running!")
